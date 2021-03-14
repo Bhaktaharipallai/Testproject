f48fb6ba253bf101c0a2cc32de0734f8c49dee26
Bhaktahari pallai <Bhaktahari pallai@DESKTOP-6PGIAAE>
2021-03-14 17:15:11 +0600

    Exception program

---
+++ ExceptionHandling/src/Exception/Exceptionerror.java
@@ -0,0 +1,14 @@
+package Exception;
+
+public class Exceptionerror {
+
+	public static void main(String[] args) {
+        System.out.println("open files");
+        int n=args.length;
+        System.out.println("n= "+ n);
+        int a=45/n;
+        System.out.println("a= "+a);
+        System.out.println("Close fies");
+	}
+
+}
